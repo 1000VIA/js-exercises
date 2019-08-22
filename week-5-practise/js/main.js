bed292531dd09e2fe5dec2c2fd73f33f91477776
1000VIA <milvampa1@gmail.com>
2019-08-22 15:21:20 -0500

    change color green jumbotron

--- week-5-practise/js/main.js
+++ week-5-practise/js/main.js
@@ -5,7 +5,7 @@ let boxBlue = document.querySelector(".jumbotron");
 btnBlue.addEventListener("click", changeThemeBlue);
 
 function changeThemeBlue() {
-  box.style.backgroundColor = "#588fbd";
+  boxBlue.style.backgroundColor = "#588fbd";
 }
 
 const btnOrange = document.querySelector("#orangeBtn");
@@ -16,4 +16,14 @@ btnOrange.addEventListener("click", changeThemeOrange);
 
 function changeThemeOrange() {
   boxOrange.style.backgroundColor = "#f0ad4e";
+}
+
+const btnGreen = document.querySelector("#greenBtn");
+
+const boxGreen = document.querySelector(".jumbotron");
+
+btnGreen.addEventListener("click", changeThemeGreen);
+
+function changeThemeGreen() {
+  boxGreen.style.backgroundColor = "#87ca8a";
 }
