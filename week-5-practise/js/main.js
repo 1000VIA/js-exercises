57f93caf8e5798c182b69d8b6810310270970a17
1000VIA <milvampa1@gmail.com>
2019-08-22 15:11:44 -0500

    change color orange jumbotron

--- week-5-practise/js/main.js
+++ week-5-practise/js/main.js
@@ -1,9 +1,19 @@
 const btnBlue = document.querySelector("#blueBtn");
 
-let box = document.querySelector(".jumbotron");
+let boxBlue = document.querySelector(".jumbotron");
 
 btnBlue.addEventListener("click", changeThemeBlue);
 
 function changeThemeBlue() {
   box.style.backgroundColor = "#588fbd";
+}
+
+const btnOrange = document.querySelector("#orangeBtn");
+
+const boxOrange = document.querySelector(".jumbotron");
+
+btnOrange.addEventListener("click", changeThemeOrange);
+
+function changeThemeOrange() {
+  boxOrange.style.backgroundColor = "#f0ad4e";
 }
