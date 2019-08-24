9ee37d80b44897fcb72bdba17ddf8612f7b8138b
1000VIA <milvampa1@gmail.com>
2019-08-23 23:14:45 -0500

    form

--- week-5-practise/js/main.js
+++ week-5-practise/js/main.js
@@ -1,3 +1,5 @@
+/*ChangeColourBlue */
+
 const btnBlue = document.querySelector("#blueBtn");
 
 let boxBlue = document.querySelector(".jumbotron");
@@ -7,6 +9,8 @@ btnBlue.addEventListener("click", changeThemeBlue);
 function changeThemeBlue() {
   boxBlue.style.backgroundColor = "#588fbd";
 }
+
+/*ChangeColourOrange */
 
 const btnOrange = document.querySelector("#orangeBtn");
 
@@ -18,6 +22,8 @@ function changeThemeOrange() {
   boxOrange.style.backgroundColor = "#f0ad4e";
 }
 
+/*ChangeColourGreen */
+
 const btnGreen = document.querySelector("#greenBtn");
 
 const boxGreen = document.querySelector(".jumbotron");
@@ -26,4 +32,30 @@ btnGreen.addEventListener("click", changeThemeGreen);
 
 function changeThemeGreen() {
   boxGreen.style.backgroundColor = "#87ca8a";
+}
+
+/*Validation form*/
+
+const btnSubmit = document.querySelector("form > .btn.btn-primary");
+const input = document.querySelectorAll(".form-control");
+const textarea = document.querySelector("textarea");
+
+btnSubmit.addEventListener("click", validateInput);
+
+function validateInput(e) {
+  e.preventDefault();
+  for (let i = 0; i < input.length; i++) {
+    if (
+      input[i].value.length === 0 ||
+      (input[0].value.includes("@") && textarea.value === "")
+    ) {
+      input[i].style.backgroundColor = "red";
+      console.log(input.value);
+    } else {
+      alert("thank you for filling out the form");
+      console.log(input.value);
+    }
+    // input.value.reset();
+  }
+  return input.value;
 }
